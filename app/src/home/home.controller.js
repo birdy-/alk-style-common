3c086f1a12d701577ee26123ddb5fe1258666f15
Antoine Durieux <adurieux@stanford.edu>
2014-06-16 16:10:52 +0200

    refactor(register) : migrated in separate file.

--- app/src/home/home.controller.js
+++ app/src/home/home.controller.js
@@ -12,25 +12,3 @@ angular.module('jDashboardFluxApp').controller('HomeCtrl', [
         };
     }
 ]);
-
-angular.module('jDashboardFluxApp').controller('RegisterCtrl', [
-    '$scope', '$$sdkCrud', '$location',
-    function ($scope, $$sdkCrud, $location) {
-        $scope.user = {
-            login: null,
-            password: null,
-            rcs: null,
-            accept: false,
-        };
-        $scope.ok = false;
-        $scope.message = null;
-        $scope.submit = function() {
-            $$sdkCrud.UserCreate($scope.user).success(function(response){
-                $scope.ok = true;
-            }).error(function(response){
-                $scope.ok = false;
-                $scope.message = "Une erreur a eu lieu pendant votre inscription, merci de réessayer ultérieurement.";
-            });
-        };
-    }
-]);
