f43b3dd19b29067f6f84eb86188447f55b27bdf1
Antoine Durieux <adurieux@stanford.edu>
2014-06-17 13:38:18 +0200

    feature(input directive) : created compact shortcuts.

--- app/src/maker/product/show/field.directive.js
+++ app/src/maker/product/show/field.directive.js
@@ -8,7 +8,7 @@ angular.module('jDashboardFluxApp').directive('productField', function() {
             pnq: '=',
             psqs: '='
         },
-        template: '<tr> <td class="pnq-name"> <label for="{{ pnq.id }}" class="control-label">{{ pnq.isConceptualizedBy.name }}</label> </td> <td ng-repeat="psq in psqs" class="psq-col"> <label ng-if="!pnq.isConceptualizedBy.compulsory" class="control-label pull-left separator"></label> <div class="input-group"> <input name="{{ pnq.id }}.quantity" type="text" class="form-control" ng-model="pnq.quantity" placeholder="ex : 100" alk-float="2" alk-focus ng-min="0" required/> <span class="input-group-addon">{{ psq.isMeasuredBy.name }} / {{ psq.name }}</span> </div> <span ng-show="false" class="help-block">La valeur entrée doit être un nombre décimal.</span> <span ng-show="false" class="help-block">La valeur entrée ne doit pas être inférieure à 0.</span> </td> <td class="pnq-legend"> </td> </tr> <tr ng-show="pnq.quantity > 0 || pnq.percentageOfDailyValueIntake"> <td class="text-right"> </td> <td ng-repeat="psq in psqs" class="psq-col"> <label class="control-label pull-left separator">soit</label> <div class="input-group"> <input name="{{ png.id }}.percentageOfDailyValueIntake" type="text" class="form-control" ng-model="pnq.percentageOfDailyValueIntake" placeholder="ex : 23.7" alk-float="2" alk-focus ng-min="0" required/> <span class="input-group-addon">% des AJR</span> </div> <span ng-show="false" class="help-block">La valeur entrée doit être un nombre décimal.</span> <span ng-show="false" class="help-block">La valeur entrée ne doit pas être inférieure à 0.</span> </td> <td class="pnq-legend"> {{ pnq.isConceptualizedBy.legend }} </td> </tr>',
+        templateUrl: '/src/maker/product/show/field.html',
         link: function(scope, element, attrs) {
             scope.$watch('pnq.quantity', function(new_, old_) {
                 if (new_ > 0) {
@@ -20,6 +20,44 @@ angular.module('jDashboardFluxApp').directive('productField', function() {
         }
     };
 });
+
+angular.module('jDashboardFluxApp').directive('inputBlockHelp', function() {
+    return {
+        restrict: 'AEC',
+        transclude: true,
+        scope: {
+            field: '='
+        },
+        templateUrl: '/src/maker/product/show/block-help.html',
+        link: function(scope, element, attrs) {
+        }
+    };
+});
+angular.module('jDashboardFluxApp').directive('inputBlockExample', function() {
+    return {
+        restrict: 'AEC',
+        transclude: true,
+        scope: {
+            field: '='
+        },
+        templateUrl: '/src/maker/product/show/block-example.html',
+        link: function(scope, element, attrs) {
+        }
+    };
+});
+angular.module('jDashboardFluxApp').directive('inputBlockGdsn', function() {
+    return {
+        restrict: 'AEC',
+        transclude: true,
+        scope: {
+            field: '='
+        },
+        templateUrl: '/src/maker/product/show/block-gdsn.html',
+        link: function(scope, element, attrs) {
+        }
+    };
+});
+
 
 
 
