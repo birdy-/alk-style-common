7bb3fb3c874b088d5f350498dcf86f311cb8223e
Antoine Durieux <adurieux@stanford.edu>
2014-06-26 00:41:30 +0200

    refactor(home) : cleanup.

--- app/src/home/home.controller.js
+++ app/src/home/home.controller.js
@@ -1,14 +1,33 @@
 'use strict';
 
 angular.module('jDashboardFluxApp').controller('HomeCtrl', [
-    '$scope', 'permission', '$location',
-    function ($scope, permission, $location) {
+    '$scope', 'permission', '$location', '$modal', '$http',
+    function ($scope, permission, $location, $modal, $http) {
         $scope.brands = [];
+        $scope.user = {};
         permission.getUser().then(function(user) {
+            $scope.user = user;
             $scope.brands = user.brandsOwned;
         });
         $scope.submit = function() {
             $location.path('/flux/maker/' + $scope.brands[0].id + '/product');
         };
+
+        var subscribe = function(){
+            var modalInstance = $modal.open({
+                templateUrl: '/src/home/mailinglist.html',
+                controller: MailingListModalController,
+                resolve: {
+                    $http: function () {
+                        return $http;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function () {
+            }, function () {
+            });
+        };
+        $scope.subscribe = subscribe;
     }
 ]);
