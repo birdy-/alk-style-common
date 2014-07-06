16be48f9091b5ccaeb25a36ac124cc8b7816d1cb
Antoine Durieux <adurieux@stanford.edu>
2014-07-06 11:30:22 +0200

    PS-65 feature(product list) : search among all brands when searching by ean.

--- app/src/maker/product/list/list.controller.js
+++ app/src/maker/product/list/list.controller.js
@@ -46,6 +46,7 @@ angular.module('jDashboardFluxApp').controller('DashboardMakerProductListCtrl', 
     // ------------------------------------------------------------------------
 
     var list = function() {
+        $log.log('List');
         if ($scope.scroll.stop) {
             $log.warn("List Products : end reached.");
             return;
@@ -54,15 +55,8 @@ angular.module('jDashboardFluxApp').controller('DashboardMakerProductListCtrl', 
             $log.warn("List Products : busy.");
             return;
         }
-        var brand = $scope.request.product.isBrandedBy;
-        if (!brand || !brand.id) {
-            $log.warn("List Products : no Brand set.");
-            return;
-        }
-        $log.log("List Products : chunk [" + $scope.scroll.offset + "-" + ($scope.scroll.offset + $scope.scroll.limit) + "]" );
-        $scope.scroll.busy = true;
 
-        // Remap parameters
+        // Collect parameters
         var certifieds = [];
         for (var key in $scope.request.product.certifieds) {
             if ($scope.request.product.certifieds[key] === true) {
@@ -74,18 +68,75 @@ angular.module('jDashboardFluxApp').controller('DashboardMakerProductListCtrl', 
         }
         $scope.request.product.certified = certifieds.join(',');
 
-        var filters = {};
-        var queries = {};
         if ($scope.request.product.isIdentifiedBy.reference) {
-            filters.isidentifiedby_reference = $scope.request.product.isIdentifiedBy.reference;
+            return findByReference();
         } else if ($scope.request.product.nameLegal) {
-            queries.namelegal = $scope.request.product.nameLegal;
-            filters.certified = $scope.request.product.certified;
+            return findByName();
         } else {
-            filters.isbrandedby_id = brand.id;
-            filters.certified = $scope.request.product.certified;
+            return findByBrand();
         }
+    };
 
+    var findByReference = function() {
+        var queries = {};
+        var filters = {
+            reference: $scope.request.product.isIdentifiedBy.reference
+        };
+        $log.log('[findByReference] '+filters.reference);
+        $scope.scroll.busy = true;
+        $$sdkCrud.ProductReferenceList(queries, filters, {},
+            $scope.scroll.offset,
+            $scope.scroll.limit
+        ).success(function(response){
+            if (response.data.length < $scope.scroll.limit) {
+                $scope.scroll.stop = true;
+            }
+            var product;
+            $log.log(response.data.length + ' results found.');
+            for (var i = 0; i < response.data.length; i ++) {
+                product = new Product().fromJson(response.data[i].identifies);
+                $scope.products.push(product);
+            }
+
+            $scope.scroll.busy = false;
+            $scope.scroll.offset = $scope.products.length;
+        });
+    };
+
+    var findByBrand = function() {
+        var brand = $scope.request.product.isBrandedBy;
+        if (!brand || !brand.id) {
+            $log.warn("[findByBrand] no Brand set.");
+            return;
+        }
+        $log.log("[findByBrand] "+brand.id);
+        var filters = {
+            isbrandedby_id: brand.id,
+            certified: $scope.request.product.certified
+        };
+        return find({}, filters);
+    };
+
+    var findByName = function() {
+        var brand = $scope.request.product.isBrandedBy;
+        if (!brand || !brand.id) {
+            $log.warn("[findByName] no Brand set.");
+            return;
+        }
+        $log.log("[findByName] "+brand.id);
+        var filters = {
+            isbrandedby_id: brand.id,
+            certified: $scope.request.product.certified
+        };
+        var queries = {
+            namelegal: $scope.request.product.nameLegal
+        }
+        return find(queries, filters);
+    };
+
+    var find = function(queries, filters) {
+        $log.log("[Find] chunk [" + $scope.scroll.offset + "-" + ($scope.scroll.offset + $scope.scroll.limit) + "]" );
+        $scope.scroll.busy = true;
         $$sdkCrud.ProductList(queries, filters, {},
             $scope.scroll.offset,
             $scope.scroll.limit
@@ -93,7 +144,7 @@ angular.module('jDashboardFluxApp').controller('DashboardMakerProductListCtrl', 
             if (response.data.length < $scope.scroll.limit) {
                 $scope.scroll.stop = true;
             }
-            var product, productInShop;
+            var product;
             for (var i = 0; i < response.data.length; i ++) {
                 product = new Product().fromJson(response.data[i]);
                 $scope.products.push(product);
@@ -107,7 +158,7 @@ angular.module('jDashboardFluxApp').controller('DashboardMakerProductListCtrl', 
 
     $scope.more = function() {
         list();
-    }
+    };
     var refresh = function() {
         $log.log('Refresh Products');
         $scope.scroll.offset = 0;
@@ -116,12 +167,12 @@ angular.module('jDashboardFluxApp').controller('DashboardMakerProductListCtrl', 
         list();
     };
 
-    // $scope.$watch('request.product.isBrandedBy', refresh);
     $scope.$watch('request.product.isIdentifiedBy.reference', refresh);
     $scope.$watch('request.product.nameLegal', refresh);
     $scope.$watch('request.product.certifieds', refresh, true);
     $scope.$watch('request.product.isBrandedBy', function() {
         $log.log('[Request watcher] Brand modified');
+        $scope.request.product.isIdentifiedBy.reference = null;
         refresh();
         var brand = $scope.request.product.isBrandedBy;
         var brandHierarchy = [];
