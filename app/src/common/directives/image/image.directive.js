90626af368bf767febddaf8fff3a302c3a3869b2
Antoine Durieux <adurieux@stanford.edu>
2014-07-06 14:57:49 +0200

    feature(image directive) : added product.

--- app/src/common/directives/image/image.directive.js
+++ app/src/common/directives/image/image.directive.js
@@ -14,9 +14,10 @@ angular.module('jDashboardFluxApp').directive('entityImage', [
             scope.$watch('entity', function(){
                 if (scope.entity._type == 'Shop') {
                     scope.url = 'https://smedia.alkemics.com/shop/'+scope.entity.id+'/picture/logo/original.png';
-                }
-                if (scope.entity._type == 'Brand') {
+                } else if (scope.entity._type == 'Brand') {
                     scope.url = 'https://smedia.alkemics.com/brand/'+scope.entity.id+'/picture/logo/original.png';
+                } else if (scope.entity._type == 'Product') {
+                    scope.url = 'https://smedia.alkemics.com/product/'+scope.entity.id+'/picture/packshot/256x256.png';
                 }
             }, true);
         }
