323299bbc08ac5fa595ceeb1ee648705382a2f13
Antoine Durieux <adurieux@stanford.edu>
2014-07-07 17:35:14 +0200

    fix(product show) : various.

--- app/src/maker/product/show/composition/composition.controller.js
+++ app/src/maker/product/show/composition/composition.controller.js
@@ -3,26 +3,4 @@
 angular.module('jDashboardFluxApp').controller('DashboardMakerProductShowCompositionCtrl', [
     '$scope',
     function ($scope) {
-
-    	// Prevents errors when clearing values
-    	$scope.$watch('product.ratioCacao', function(){
-    		if ($scope.product.ratioCacao === '') {
-    			$scope.product.ratioCacao = null;
-    		}
-    	});
-    	$scope.$watch('product.ratioAlcohol', function(){
-    		if ($scope.product.ratioAlcohol === '') {
-    			$scope.product.ratioAlcohol = null;
-    		}
-    	});
-    	$scope.$watch('product.ratioFat', function(){
-    		if ($scope.product.ratioFat === '') {
-    			$scope.product.ratioFat = null;
-    		}
-    	});
-    	$scope.$watch('product.levelPH', function(){
-    		if ($scope.product.levelPH === '') {
-    			$scope.product.levelPH = null;
-    		}
-    	});
 }]);
