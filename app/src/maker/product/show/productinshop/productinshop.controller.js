4092f07e1dc055927a50feda2ebe06e8fd15365a
Antoine DURIEUX <ad@alkemics.com>
2014-09-23 11:58:04 +0200

    fix(misc) : IE compatibility.

--- app/src/maker/product/show/productinshop/productinshop.controller.js
+++ app/src/maker/product/show/productinshop/productinshop.controller.js
@@ -4,19 +4,19 @@ angular.module('jDashboardFluxApp').controller('DashboardMakerProductShowProduct
     '$scope', '$$sdkCrud',
     function ($scope, $$sdkCrud) {
 
-    	$scope.productInShops = [];
+        $scope.productInShops = [];
 
-    	var partners = function(productInShop) {
-    		return [1, 2, 7, 10, 67, 65, 74, 75, 12].indexOf(productInShop.isSoldBy.id) !== -1;
-    	};
+        var partners = function(productInShop) {
+            return [1, 2, 7, 10, 67, 65, 74, 75, 12].indexOf(productInShop.isSoldBy.id) !== -1;
+        };
 
         $scope.$watch('product.id', function(productId){
-        	if (!productId) {
-        		return;
-        	}
-        	$$sdkCrud.ProductIsInstantiatedBy(productId).then(function(response){
-        		$scope.productInShops = response.data.data.isInstantiatedBy.filter(partners);
-        	});
+            if (!productId) {
+                return;
+            }
+            $$sdkCrud.ProductIsInstantiatedBy(productId).then(function(response){
+                $scope.productInShops = response.data.data.isInstantiatedBy.filter(partners);
+            });
         });
 
 }]);
