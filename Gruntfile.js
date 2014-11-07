6409850c960e663d3b09cfa5c37d941e64fdbe75
Adlen Afane <aa@alkemics.com>
2014-11-07 11:56:59 +0100

    Feat(test): Protractor conf

--- Gruntfile.js
+++ Gruntfile.js
@@ -508,7 +508,7 @@ module.exports = function (grunt) {
     'clean:server',
     'concurrent:test',
     'autoprefixer',
-    'connect:test',
+    // 'connect:test',
     'karma',
     'protractor_webdriver',
     'protractor'
