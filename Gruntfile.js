bbceb98305596ae315060647f6a46e985058dbdd
aperrin <perrin18.antoine@gmail.com>
2014-11-15 22:15:25 +0100

    fake1

--- Gruntfile.js
+++ Gruntfile.js
@@ -541,7 +541,7 @@ module.exports = function (grunt) {
     // 'uglify',// Do not compress for preprod debug
     'rev',
     'usemin',
-    // 'sosie',
+    'sosie',
     'copy:prod',
     'shell:compress'
   ]);
