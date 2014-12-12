afe6f329ba089e8a168026fd2eec4222d345744c
Adlen Afane <aa@alkemics.com>
2014-12-12 14:24:14 +0100

    Fix(devops): Remove non functional slackistrano

--- capistrano/config/deploy.rb
+++ capistrano/config/deploy.rb
@@ -42,19 +42,6 @@ set :log_level, :debug
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-set :slack_team, "alkemics"
-set :slack_token, "xoxp-2722465896-2945065910-3034863114-8cf50a"
-
-set :slack_channel,      ->{ '#front' }
-set :slack_username,     ->{ 'Slackistrano - dashboard-flux' }
-set :slack_run_starting, ->{ true }
-set :slack_run_finished, ->{ true }
-set :slack_run_failed,   ->{ true }
-set :slack_msg_starting, ->{ "#{ENV['USER'] || ENV['USERNAME']} has started deploying branch #{fetch :branch} of #{fetch :application} to #{fetch :stage, 'unknown stage'}." }
-set :slack_msg_finished, ->{ "#{ENV['USER'] || ENV['USERNAME']} has finished deploying branch #{fetch :branch} of #{fetch :application} to #{fetch :stage, 'unknown stage'}." }
-set :slack_msg_failed,   ->{ "*ERROR!* #{ENV['USER'] || ENV['USERNAME']} failed to deploy branch #{fetch :branch} of #{fetch :application} to #{fetch :stage, 'unknown stage'}." }
-set :slack_via_slackbot, ->{ true } # Set to true to send the message via slackbot instead of webhook
-
 namespace :alk do
 
     desc 'Full Deploy'
