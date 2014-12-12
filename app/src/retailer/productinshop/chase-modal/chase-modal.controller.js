618d2212b9b541cf2c162a5920b5437a2625167f
Adlen Afane <aa@alkemics.com>
2014-12-12 12:26:46 +0100

    [PS-376]Feat(retail): message chase

---
+++ app/src/retailer/productinshop/chase-modal/chase-modal.controller.js
@@ -0,0 +1,45 @@
+'use_strict';
+
+/**
+ * Modal that allows the user to accept the responsability for a given productInShop.
+ */
+angular.module('jDashboardFluxApp').controller('ProductChaseModalController', [
+    '$scope', '$modalInstance', '$window', 'productInShops', 'user', '$$sdkMailer',
+    function ($scope, $modalInstance, $window, productInShops, user, $$sdkMailer) {
+
+    // ------------------------------------------------------------------------
+    // Variables
+    // ------------------------------------------------------------------------
+        $scope.productInShops = productInShops;
+        $scope.message = {
+            from: user,
+            to: {
+                username: null
+            },
+            subject: '[INCO] ' + user.firstname + ' ' + user.lastname + ' vous invite à modifier vos fiches produit sur Alkemics',
+            data: {
+                productInShops: productInShops
+            }
+        };
+
+    // ------------------------------------------------------------------------
+    // Event binding
+    // ------------------------------------------------------------------------
+        $scope.ok = function () {
+            $$sdkMailer.RetailerProductDataCompletionInvitationPost($scope.message).success(function (response) {
+                $window.alert('Le message a bien été envoyé.');
+                $modalInstance.close();
+            }).error(function(){
+                $window.alert('Une erreur est survenue pendant l\'envoi de l\'email. Merci de réessayer ultérieurement ou de contacter notre support.');
+            });
+        };
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+
+
+    // ------------------------------------------------------------------------
+    // Init
+    // ------------------------------------------------------------------------
+    }
+]);
