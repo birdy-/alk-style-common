6c79720da62fbead56eb5078f2d0b76d7bd0a564
Adlen Afane <aa@alkemics.com>
2015-01-19 14:22:38 +0100

    [API-297]Feat(BIN): Checkbox list instead of autocomplete

--- app/src/dmp/activation/buy-it-now/show-controller.js
+++ app/src/dmp/activation/buy-it-now/show-controller.js
@@ -7,10 +7,18 @@ angular.module('jDashboardFluxApp').controller('DmpActivationBuyItNowShowControl
     // ------------------------------------------------------------------------
     // Variables
     // ------------------------------------------------------------------------
+    var authorizedShopIds = [2, 4, 7, 13, 14, 38];
+
     $scope.campaign = null;
     $scope.user = null;
     $scope.preview = false;
     $scope.restrict = { with_isidentifiedby: 1 };
+    $scope.authorizedShops = [];
+    $scope.selectedShops = [];
+
+    // ------------------------------------------------------------------------
+    // Helpers
+    // ------------------------------------------------------------------------
 
     var computeAvailableShops = function (shops) {
         var shopIds = [];
@@ -20,6 +28,20 @@ angular.module('jDashboardFluxApp').controller('DmpActivationBuyItNowShowControl
         return shopIds
     };
 
+    var fetchAuthorizedShop = function (id) {
+        $$ORM.repository('Shop').get(id).then(function (shop) {
+            $scope.authorizedShops.push(shop);
+        });
+    };
+
+    var fetchSelectedShop = function (id) {
+        angular.forEach($scope.authorizedShops, function (shop) {
+            if (shop.id === id) {
+                $scope.campaign.parameters.availableShops.push(shop);
+            }
+        });
+    };
+
     // ------------------------------------------------------------------------
     // Event binding
     // ------------------------------------------------------------------------
@@ -27,6 +49,9 @@ angular.module('jDashboardFluxApp').controller('DmpActivationBuyItNowShowControl
         // Compute campaign parameters
         $scope.campaign.runsOnProduct = $scope.campaign._runsOnProduct;
         $scope.campaign.runsIn = [$scope.campaign._runsIn];
+        $scope.campaign.parameters.availableShops = [];
+        $log.info('Selected shops:', $scope.selectedShops);
+        angular.forEach($scope.selectedShops, fetchSelectedShop);
 
         // Error callback
         var error = function (response) {
@@ -44,6 +69,18 @@ angular.module('jDashboardFluxApp').controller('DmpActivationBuyItNowShowControl
                 $scope.campaign = campaign;
                 $scope.preview = true;
             }, error);
+        }
+    };
+
+    $scope.toggleSelection = function toggleSelection(shopId) {
+        var ids = $scope.selectedShops.indexOf(shopId);
+        // is currently selected
+        if (ids > -1) {
+          $scope.selectedShops.splice(ids, 1);
+        }
+        // is newly selected
+        else {
+          $scope.selectedShops.push(shopId);
         }
     };
 
@@ -74,6 +111,12 @@ angular.module('jDashboardFluxApp').controller('DmpActivationBuyItNowShowControl
                 }
             }
 
+            if (campaign.parameters.availableShops.length) {
+                angular.forEach(campaign.parameters.availableShops, function (shop) {
+                    $scope.toggleSelection(shop.id);
+                });
+            }
+
             $scope.campaign = campaign;
             $scope.preview = true;
         }, function (response) {
@@ -90,6 +133,8 @@ angular.module('jDashboardFluxApp').controller('DmpActivationBuyItNowShowControl
                 create();
             }
         });
+
+        angular.forEach(authorizedShopIds, fetchAuthorizedShop);
     }
     init();
 
