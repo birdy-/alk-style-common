f7a0823e528aa6d14489ac7529e0f6ca97e39928
Adlen Afane <adlen.afane@gmail.com>
2015-02-21 19:48:59 +0100

    [PS-580]Feat(maker): Added list view and multiple certificate

--- app/src/maker/product/certify/certify.controller.js
+++ app/src/maker/product/certify/certify.controller.js
@@ -4,27 +4,30 @@
  * Modal that allows the user to certify a given product.
  */
 angular.module('jDashboardFluxApp').controller('ProductCertificationModalController', [
-    '$scope', '$modalInstance', '$$sdkCrud', '$window', 'product', 'user',
-    function ($scope, $modalInstance, $$sdkCrud, $window, product, user) {
+    '$scope', '$modalInstance', '$$sdkCrud', '$window', 'products', 'user',
+    function ($scope, $modalInstance, $$sdkCrud, $window, products, user) {
 
-    $scope.product = product;
+    $scope.products = products;
     $scope.user = user;
 
     $scope.ok = function () {
         if (!$scope.user.email) {
             return;
         }
-        $scope.product.certified = Product.CERTIFICATION_STATUS_CERTIFIED.id;
-        $$sdkCrud.ProductCertify(
-            $scope.product,
-            Product.CERTIFICATION_STATUS_CERTIFIED.id,
-            "1169"
-        ).success(function(response){
-            $scope.product.certified = response.data.certified;
-            $modalInstance.close($scope.product);
-        }).error(function(response){
-            $window.alert("Erreur pendant la certification du produit : " + response.data.message);
-        });
+        for (var i=0 in products) {
+            var product = products[i];
+            product.certified = Product.CERTIFICATION_STATUS_CERTIFIED.id;
+            $$sdkCrud.ProductCertify(
+                product,
+                Product.CERTIFICATION_STATUS_CERTIFIED.id,
+                "1169"
+            ).success(function(response){
+                product.certified = response.data.certified;
+            }).error(function(response){
+                $window.alert("Erreur pendant la certification du produit : " + response.data.message);
+            });
+        }
+        $modalInstance.close(products);
     };
     $scope.cancel = function () {
         $modalInstance.dismiss('cancel');
