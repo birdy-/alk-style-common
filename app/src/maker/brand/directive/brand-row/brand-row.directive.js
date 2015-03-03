6c6757016a673ade5061209141d95c6b9e80e1d9
Adlen Afane <adlen.afane@gmail.com>
2015-03-04 00:53:18 +0100

    Feat(UI): Brand page

--- app/src/maker/brand/directive/brand-row/brand-row.directive.js
+++ app/src/maker/brand/directive/brand-row/brand-row.directive.js
@@ -1,17 +1,40 @@
 'use strict';
 
 angular.module('jDashboardFluxApp').directive('brandRow', [
-    '$modal',
-    function ($modal) {
+    '$rootScope', '$location',
+    function ($rootScope, $location) {
         return {
-            restrict: 'AC',
+            restrict: 'AEC',
             scope: {
               brand: '='
             },
             templateUrl: '/src/maker/brand/directive/brand-row/brand-row.view.html',
             link: function($scope, $elem, $attrs) {
-              $scope.notCertified = parseInt($scope.brand.stats.counts['5'], 10) + parseInt($scope.brand.stats.counts['1'], 10) || 0;
-              $scope.certified = parseInt($scope.brand.stats.counts['2']) || 0
+                $scope.notCertified = parseInt($scope.brand.stats.counts['5'], 10) + parseInt($scope.brand.stats.counts['1'], 10) || 0;
+                $scope.certified = parseInt($scope.brand.stats.counts['2']) || 0
+
+                $scope.goToProducts = function (brandId, certified) {
+                    $rootScope.navigation.maker.request = {
+                        product: {
+                            isIdentifiedBy: {},
+                            certifieds: []
+                        }
+                    };
+
+                    if (certified) {
+                        $rootScope.navigation.maker.request.product.certifieds[Product.CERTIFICATION_STATUS_CERTIFIED.id] = true;
+                    } else {
+                        $rootScope.navigation.maker.request.product.certifieds[Product.CERTIFICATION_STATUS_ATTRIBUTED.id] = true;
+                        $rootScope.navigation.maker.request.product.certifieds[Product.CERTIFICATION_STATUS_ACCEPTED.id] = true;
+                    }
+
+                    $rootScope.navigation.maker.request.offset = 0;
+                    $rootScope.navigation.maker.request.limit = 24;
+                    $rootScope.navigation.maker.request.busy = false;
+
+                    $rootScope.navigation.maker.request.initialized = true;
+                    $location.path('/maker/brand/' + brandId + '/product');
+              };
             }
         };
     }
