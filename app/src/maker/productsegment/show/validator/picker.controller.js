ad1f88fb06e9380cc276e30fccd1d059db9f1fd5
Flo <florent.marima@gmail.com>
2015-04-02 12:03:21 +0200

    add products validator screen

---
+++ app/src/maker/productsegment/show/validator/picker.controller.js
@@ -0,0 +1,25 @@
+'use_strict';
+
+angular.module('jDashboardFluxApp').controller('ProductSegmentValidatorPickerController', [
+    '$scope', '$$autocomplete', '$location', '$window', 'URL_CDN_MEDIA', '$modalInstance', 'permission', 'data',
+    function ($scope, $$autocomplete, $location, $window, URL_CDN_MEDIA, $modalInstance, permission, data) {
+
+    // ------------------------------------------------------------------------
+    // Variables
+    // ------------------------------------------------------------------------
+    $scope.data = data;
+    $scope.response = {};
+
+    // ------------------------------------------------------------------------
+    // Event Binding
+    // ------------------------------------------------------------------------
+    $scope.cancel = function () {
+        $modalInstance.dismiss(null);
+    };
+
+    $scope.submit = function () {
+        console.log($scope.response);
+        $modalInstance.close($scope.response);
+    };
+
+}]);
