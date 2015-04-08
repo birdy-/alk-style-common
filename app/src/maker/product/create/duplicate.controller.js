145c08e9eebf50d4e45711b6eacda4b669f54142
Adlen Afane <aa@alkemics.com>
2015-04-08 15:57:12 +0200

    [PS-364]Test(maker): Added some tests on actions

--- app/src/maker/product/create/duplicate.controller.js
+++ app/src/maker/product/create/duplicate.controller.js
@@ -11,38 +11,6 @@ angular.module('jDashboardFluxApp')
     $scope.product = product;
     $scope.user = user;
 
-    var formatProductReference = function (response) {
-        // If the GTIN is incoherent (too few digits, incoherent verification digit)
-        if (response.data.message && response.data.message.indexOf("is not valid") !== -1) {
-            $log.error('Bad reference.');
-            return;
-        }
-
-        if (response.data.data.length === 0) {
-            $log.error('No reference found.');
-            return;
-        }
-
-        var productId = response.data.data[0].identifies.id;
-        $$sdkCrud.ProductShow(productId).then(function(response){
-            var product = response.data.data;
-            // Else everything is fine
-            $log.info('Product found:', product);
-            // Claim will be sent when the user validates.
-        }, function (response) {
-            $window.alert("Erreur pendant la récupération du produit : " + response.data.message);
-        });
-    };
-
-    $scope.searchProductReference = function (reference) {
-        $$sdkCrud.ProductReferenceList({}, {
-            reference: reference,
-            type: ProductReference.TYPE_EAN13.name
-        }).then(formatProductReference, function (response) {
-            $window.alert("Erreur pendant la recherche de l'EAN : " + response.data.message);
-        });
-    };
-
     $scope.ok = function (product, reference) {
         var payload = {
             reference: reference,
