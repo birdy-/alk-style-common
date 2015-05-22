09ff359d7dd68bae90ebc10b5063da88401b2dc2
Adlen Afane <aa@alkemics.com>
2015-05-22 14:05:32 +0200

    [PS-937]Feat(UI): Lots of improvements

--- app/src/organization/admin/home/list.controller.js
+++ app/src/organization/admin/home/list.controller.js
@@ -100,6 +100,12 @@ angular.module('jDashboardFluxApp').controller('OrganizationAdminHomeListControl
         });
     };
 
+    $scope.toggleSearchDisplay = function () {
+        // set focus on search
+        $scope.displaySearch = !$scope.displaySearch;
+        return;
+    }
+
     // --------------------------------------------------------------------------------
     // Init
     // --------------------------------------------------------------------------------
@@ -120,7 +126,6 @@ angular.module('jDashboardFluxApp').controller('OrganizationAdminHomeListControl
 
         $$ORM.repository('ProductSegment').list(query, {}, {}, 0, 100).then(function (segments) {
             $scope.segments = segments;
-            console.log(segments);
             $scope.selectedSegment = $scope.selectSegment($scope.segments[0].id);
 
             var productSegmentRoot = Organization.getProductSegmentRoot($scope.organization);
@@ -155,4 +160,27 @@ angular.module('jDashboardFluxApp').controller('OrganizationAdminHomeListControl
         });
     };
     init();
-}]);
+}])
+
+.directive('focusMe', function ($timeout) {
+  return {
+    link: function($scope, $element, $attrs) {
+        $scope.$watch($attrs.focusMe, function (value) {
+            if(value === true) {
+                $element[0].focus();
+            }
+        });
+
+        $element.bind('blur', function () {
+            if (!$scope.search) {
+                $scope.search = {};
+            }
+            if (!$scope.filteredSegments.length || !$scope.search.$) {
+                $scope.search.$ = '';
+                $scope[$attrs.focusMe] = false;
+                $scope.$apply();
+            }
+        });
+    }
+  };
+});
