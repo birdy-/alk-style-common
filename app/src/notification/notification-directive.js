d73f178d7301bb4133f5178761bca295b6fc7e8f
Adlen Afane <aa@alkemics.com>
2015-05-25 09:43:59 +0200

    [PS-900]Feat(): POC Carrefour Notification

--- app/src/notification/notification-directive.js
+++ app/src/notification/notification-directive.js
@@ -37,12 +37,16 @@ angular.module('jDashboardFluxApp').directive('alkNotification', [
             'RetailerProductPictureAsked': 'fa-info-circle',
             'RetailerProductUpdatedByMaker': 'fa-check-circle-o',
             'RetailerProductUpdatedByPhotographer': 'fa-check-circle-o',
+            'RetailerProductUploadedByPhotographer': 'fa-check-circle-o',
 
             'MakerProductClaimByRetailer': 'fa-info-circle',
             'MakerProductTechnicalValidation': 'fa-info-circle',
             'MakerProductArtisticValidation': 'fa-check-circle-o',
             'MakerProductArtisticClaim': 'fa-info-circle',
-            'MakerProductValidation': 'fa-check-circle-o'
+            'MakerProductValidation': 'fa-check-circle-o',
+
+            'PhotographerProductValidation': 'fa-info-circle',
+            'PhotographerProductPictureClaim': 'fa-info-circle'
         };
         var color = {
             'BrandClaimAccepted': 'color-green-inverse',
@@ -74,12 +78,16 @@ angular.module('jDashboardFluxApp').directive('alkNotification', [
             'RetailerProductPictureAsked': 'color-blue-inverse',
             'RetailerProductUpdatedByMaker': 'color-green-inverse',
             'RetailerProductUpdatedByPhotographer': 'color-green-inverse',
+            'RetailerProductUploadedByPhotographer': 'color-green-inverse',
 
             'MakerProductClaimByRetailer': 'color-orange-inverse',
             'MakerProductTechnicalValidation': 'color-blue-inverse',
             'MakerProductArtisticValidation': 'color-green-inverse',
             'MakerProductArtisticClaim': 'color-orange-inverse',
-            'MakerProductValidation': 'color-green-inverse'
+            'MakerProductValidation': 'color-green-inverse',
+
+            'PhotographerProductValidation': 'color-blue-inverse',
+            'PhotographerProductPictureClaim': 'color-blue-inverse'
         };
 
         return {
@@ -87,41 +95,208 @@ angular.module('jDashboardFluxApp').directive('alkNotification', [
             scope: {
                 notification: '=alkNotification'
             },
-            controller: function ($scope, $$sdkTimeline, permission) {
-                $scope.sendAnswer = function(notification) {
-                    permission.getUser().then(function (user) {
-                        var new_event = {
-                            'entity_id': notification.event.entity_id,
-                            'timestamp': moment().unix(),
-                            'type': 'Discussion',
-                            'user_id': user.id,
-                            'allow_response': true,
-                            'data': {
-                                'message': notification.event.answer,
-                                'subject': '',
-                                'timestamp': Math.floor(Date.now() / 1000),
+            controller: [
+                '$scope', '$$sdkTimeline', 'permission', 'ngToast',
+                function ($scope, $$sdkTimeline, permission, ngToast) {
+
+                    var makerUserId = 842;
+                    var retailerUserId = 857;
+                    var photographerUserId = 891;
+
+                    $scope.sendAnswer = function(notification) {
+                        permission.getUser().then(function (user) {
+                            var new_event = {
                                 'entity_id': notification.event.entity_id,
+                                'timestamp': moment().unix(),
                                 'type': 'Discussion',
-                                'user': {
-                                    'firstname': user.firstname,
-                                    'id': user.id,
-                                    'lastname': user.lastname,
-                                    'username': user.username
+                                'user_id': user.id,
+                                'allow_response': true,
+                                'data': {
+                                    'message': notification.event.answer,
+                                    'subject': '',
+                                    'timestamp': Math.floor(Date.now() / 1000),
+                                    'entity_id': notification.event.entity_id,
+                                    'type': 'Discussion',
+                                    'user': {
+                                        'firstname': user.firstname,
+                                        'id': user.id,
+                                        'lastname': user.lastname,
+                                        'username': user.username
+                                    }
                                 }
-                            }
-                        };
-                        $$sdkTimeline.SendEvent({
-                            'entity_type': 'Discussion',
-                            'entity_id': notification.event.entity_id,
-                            'event': new_event,
-                            'user_id': user.id,
-                        }).then(function (response) {
-                            notification.event.displayAnswer = false;
-                            location.reload();
+                            };
+                            $$sdkTimeline.SendEvent({
+                                'entity_type': 'Discussion',
+                                'entity_id': notification.event.entity_id,
+                                'event': new_event,
+                                'user_id': user.id,
+                            }).then(function (response) {
+                                notification.event.displayAnswer = false;
+                                location.reload();
+                            });
                         });
-                    });
-                };
-            },
+                    };
+
+                    $scope.claimPictureToRetailer = function (notification) {
+                        permission.getUser().then(function (user) {
+                            // Warn the retailer
+                            var newEvent = {
+                                entity_type: 'RetailerProductPictureAsked',
+                                timestamp: moment().unix(),
+                                type: 'RetailerProductPictureAsked',
+                                data: notification.event.data
+                            };
+
+                            $$sdkTimeline.SendEvent({
+                                entity_id: moment().unix(),
+                                entity_type: 'RetailerProductPictureAsked',
+                                user_id: retailerUserId,
+                                event: newEvent
+                            }).then(function (response) {
+                                ngToast.create({
+                                    className: 'success',
+                                    content: 'Votre message a bien été envoyé.'
+                                });
+                            });
+
+                            // Ask more info to maker
+                            var newEvent = {
+                                entity_type: 'MakerProductArtisticClaim',
+                                timestamp: moment().unix(),
+                                type: 'MakerProductArtisticClaim',
+                                data: notification.event.data
+                            };
+
+                            $$sdkTimeline.SendEvent({
+                                entity_id: moment().unix(),
+                                entity_type: 'MakerProductArtisticClaim',
+                                user_id: makerUserId,
+                                event: newEvent
+                            }).then(function (response) {
+                                ngToast.create({
+                                    className: 'success',
+                                    content: 'Votre message a bien été envoyé.'
+                                });
+                            });
+                        });
+
+                        $scope.warnPhotographerProductSent(notification);
+                    };
+
+                    $scope.warnPhotographerProductSent = function (notification) {
+                        // Warn photographer
+                        var newEvent = {
+                            entity_type: 'PhotographerProductPictureClaim',
+                            timestamp: moment().unix(),
+                            type: 'PhotographerProductPictureClaim',
+                            data: notification.event.data
+                        };
+
+                        $$sdkTimeline.SendEvent({
+                            entity_id: moment().unix(),
+                            entity_type: 'PhotographerProductPictureClaim',
+                            user_id: photographerUserId,
+                            event: newEvent
+                        }).then(function (response) {
+                            ngToast.create({
+                                className: 'success',
+                                content: 'Votre message a bien été envoyé.'
+                            });
+                        });
+                    };
+
+                    $scope.validatePictureArtistic = function (notification) {
+                        permission.getUser().then(function (user) {
+                            var newEvent = {
+                                entity_type: 'MakerProductArtisticValidation',
+                                timestamp: moment().unix(),
+                                type: 'MakerProductArtisticValidation',
+                                data: notification.event.data
+                            };
+
+                            $$sdkTimeline.SendEvent({
+                                entity_id: moment().unix(),
+                                entity_type: 'MakerProductArtisticValidation',
+                                user_id: makerUserId,
+                                event: newEvent
+                            }).then(function (response) {
+                                ngToast.create({
+                                    className: 'success',
+                                    content: 'Votre message a bien été envoyé.'
+                                });
+                            });
+
+                            var newEvent = {
+                                entity_type: 'RetailerProductUpdatedByMaker',
+                                timestamp: moment().unix(),
+                                type: 'RetailerProductUpdatedByMaker',
+                                data: notification.event.data
+                            };
+
+                            $$sdkTimeline.SendEvent({
+                                entity_id: moment().unix(),
+                                entity_type: 'RetailerProductUpdatedByMaker',
+                                user_id: retailerUserId,
+                                event: newEvent
+                            }).then(function (response) {
+                                ngToast.create({
+                                    className: 'success',
+                                    content: 'Votre message a bien été envoyé.'
+                                });
+                            });
+                        });
+                    };
+
+                    $scope.rejectPictureArtistic = function (notification) {
+                        $scope.claimPictureToRetailer(notification);
+                    };
+
+                    $scope.chaseProductInformation = function (notification) {
+                        permission.getUser().then(function (user) {
+                            var newEvent = {
+                                entity_type: 'MakerProductClaimByRetailer',
+                                timestamp: moment().unix(),
+                                type: 'MakerProductClaimByRetailer',
+                                data: notification.event.data
+                            };
+
+                            $$sdkTimeline.SendEvent({
+                                entity_id: moment().unix(),
+                                entity_type: 'MakerProductClaimByRetailer',
+                                user_id: makerUserId,
+                                event: newEvent
+                            }).then(function (response) {
+                                ngToast.create({
+                                    className: 'success',
+                                    content: 'Votre message a bien été envoyé.'
+                                });
+                            });
+                        });
+                    };
+
+                    $scope.chaseProductReception = function (notification) {
+                        // Ask more info to maker
+                        var newEvent = {
+                            entity_type: 'MakerProductArtisticClaim',
+                            timestamp: moment().unix(),
+                            type: 'MakerProductArtisticClaim',
+                            data: notification.event.data
+                        };
+
+                        $$sdkTimeline.SendEvent({
+                            entity_id: moment().unix(),
+                            entity_type: 'MakerProductArtisticClaim',
+                            user_id: makerUserId,
+                            event: newEvent
+                        }).then(function (response) {
+                            ngToast.create({
+                                className: 'success',
+                                content: 'Votre message a bien été envoyé.'
+                            });
+                        });
+                    };
+                }
+            ],
             templateUrl: '/src/notification/notification-directive.html',
             link: function(scope, elem, attrs) {
                 scope.notification.icon = icon[scope.notification.event.type];
--- app/src/retailer/notification/list-controller.js
+++ app/src/retailer/notification/list-controller.js
@@ -24,90 +24,107 @@ angular.module('jDashboardFluxApp').controller('DashboardRetailerNotificationLis
         // ------------------------------------------------------------------------
         // Init
         // ------------------------------------------------------------------------
-        permission.getUser().then(function(user){
-            $$sdkTimeline.TimelineGet(user.id).then(function(response) {
+        permission.getUser().then(function (user) {
+            $$sdkTimeline.TimelineGet(user.id).then(function (response) {
                 $scope.notifications = $scope.notifications.concat(response.data.data);
-            });
-        });
 
-        // POC Carrefour
-        $scope.notifications.push({
-            'event': {
-                timestamp: moment().unix(),
-                type: 'RetailerProductAvailable',
-                data: {
-                    claimDate: moment().set({'year': 2015, 'month': 5, 'day': 4}).format('DD MMM YYYY'),
-                    product: {
-                        reference: '3663215010508',
-                        name: 'Smoothie Orange Bio 1L',
-                        manufacturerName: 'Alkemics Brand',
-                        photographerName: 'ProductPhoto',
-                        urlPicture: 'https://smedia.alkemics.com/product/387085/picture/packshot/256x256.png'
+                if (+user.id !== 10 && +user.id !== 857) { return; }
+
+                // POC Carrefour
+                $scope.notifications.push({
+                    'event': {
+                        timestamp: 1432539300,
+                        type: 'RetailerProductAvailable',
+                        data: {
+                            claimDate: moment().set({'year': 2015, 'month': 4, 'date': 27}).format('DD MMM YYYY'),
+                            claimer: {
+                                name: 'Carrefour',
+                                photographer: {
+                                    name: 'ProductPhoto',
+                                    address: '8 rue du Sentier, 75002 Paris'
+                                }
+                            },
+                            product: {
+                                reference: '3663215010508',
+                                name: 'Smoothie Orange Bio 1L',
+                                manufacturerName: 'Alkemics Brand',
+                                photographerName: 'ProductPhoto',
+                                urlPicture: 'https://smedia.alkemics.com/product/387085/picture/packshot/256x256.png'
+                            }
+                        }
                     }
-                }
-            }
-        });
-        $scope.notifications.push({
-            event: {
-                timestamp: moment().unix(),
-                type: 'RetailerProductNotAvailable',
-                data: {
-                    claimDate: moment().set({'year': 2015, 'month': 5, 'day': 4}).format('DD MMM YYYY'),
-                    product: {
-                        reference: '3663215010508',
-                        name: 'Smoothie Orange Bio 1L',
-                        manufacturerName: 'Alkemics Brand',
-                        photographerName: 'ProductPhoto'
+                });
+                $scope.notifications.push({
+                    event: {
+                        timestamp: 1432539321,
+                        type: 'RetailerProductNotAvailable',
+                        data: {
+                            claimDate: moment().set({'year': 2015, 'month': 4, 'date': 27}).format('DD MMM YYYY'),
+                            claimer: {
+                                name: 'Carrefour',
+                                photographer: {
+                                    name: 'ProductPhoto',
+                                    address: '8 rue du Sentier, 75002 Paris'
+                                }
+                            },
+                            product: {
+                                reference: '3663215010508',
+                                name: 'Smoothie Orange Bio 1L',
+                                manufacturerName: 'Alkemics Brand',
+                                photographerName: 'ProductPhoto',
+                                urlPicture: 'https://smedia.alkemics.com/product/387085/picture/packshot/256x256.png'
+                            }
+                        }
                     }
-                }
-            }
-        });
-        $scope.notifications.push({
-            event: {
-                timestamp: moment().unix(),
-                type: 'RetailerProductPictureAsked',
-                data: {
-                    claimDate: moment().set({'year': 2015, 'month': 5, 'day': 4}).format('DD MMM YYYY'),
-                    product: {
-                        reference: '3663215010508',
-                        name: 'Smoothie Orange Bio 1L',
-                        manufacturerName: 'Alkemics Brand',
-                        photographerName: 'ProductPhoto'
-                    }
-                }
-            }
-        });
-        $scope.notifications.push({
-            event: {
-                timestamp: moment().unix(),
-                type: 'RetailerProductUpdatedByMaker',
-                data: {
-                    claimDate: moment().set({'year': 2015, 'month': 5, 'day': 4}).format('DD MMM YYYY'),
-                    product: {
-                        reference: '3663215010508',
-                        name: 'Smoothie Orange Bio 1L',
-                        manufacturerName: 'Alkemics Brand',
-                        photographerName: 'ProductPhoto',
-                        urlPicture: 'https://smedia.alkemics.com/product/387085/picture/packshot/256x256.png'
-                    }
-                }
-            }
-        });
-        $scope.notifications.push({
-            event: {
-                timestamp: moment().unix(),
-                type: 'RetailerProductUpdatedByPhotographer',
-                data: {
-                    claimDate: moment().set({'year': 2015, 'month': 5, 'day': 4}).format('DD MMM YYYY'),
-                    product: {
-                        reference: '3663215010508',
-                        name: 'Smoothie Orange Bio 1L',
-                        manufacturerName: 'Alkemics Brand',
-                        photographerName: 'ProductPhoto',
-                        urlPicture: 'https://smedia.alkemics.com/product/387085/picture/packshot/256x256.png'
-                    }
-                }
-            }
+                });
+                // $scope.notifications.push({
+                //     event: {
+                //         timestamp: moment().unix(),
+                //         type: 'RetailerProductPictureAsked',
+                //         data: {
+                //             claimDate: moment().set({'year': 2015, 'month': 5, 'day': 4}).format('DD MMM YYYY'),
+                //             product: {
+                //                 reference: '3663215010508',
+                //                 name: 'Smoothie Orange Bio 1L',
+                //                 manufacturerName: 'Alkemics Brand',
+                //                 photographerName: 'ProductPhoto'
+                //             }
+                //         }
+                //     }
+                // });
+                // $scope.notifications.push({
+                //     event: {
+                //         timestamp: moment().unix(),
+                //         type: 'RetailerProductUpdatedByMaker',
+                //         data: {
+                //             claimDate: moment().set({'year': 2015, 'month': 5, 'day': 4}).format('DD MMM YYYY'),
+                //             product: {
+                //                 reference: '3663215010508',
+                //                 name: 'Smoothie Orange Bio 1L',
+                //                 manufacturerName: 'Alkemics Brand',
+                //                 photographerName: 'ProductPhoto',
+                //                 urlPicture: 'https://smedia.alkemics.com/product/387085/picture/packshot/256x256.png'
+                //             }
+                //         }
+                //     }
+                // });
+                // $scope.notifications.push({
+                //     event: {
+                //         timestamp: moment().unix(),
+                //         type: 'RetailerProductUpdatedByPhotographer',
+                //         data: {
+                //             claimDate: moment().set({'year': 2015, 'month': 5, 'day': 4}).format('DD MMM YYYY'),
+                //             product: {
+                //                 reference: '3663215010508',
+                //                 name: 'Smoothie Orange Bio 1L',
+                //                 manufacturerName: 'Alkemics Brand',
+                //                 photographerName: 'ProductPhoto',
+                //                 urlPicture: 'https://smedia.alkemics.com/product/387085/picture/packshot/256x256.png'
+                //             }
+                //         }
+                //     }
+                // });
+            });
         });
     }
 ]);
