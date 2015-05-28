2a920805ce6c9c4ceb3eff40d1f9222fdd1ebd18
Clément Danjou <cdanjou@alkemics.com>
2015-05-28 15:40:33 +0200

    Enhance display of tags

--- app/src/retailer/productinshop/chase-modal/chase-modal.controller.js
+++ app/src/retailer/productinshop/chase-modal/chase-modal.controller.js
@@ -4,29 +4,22 @@
  * Modal that allows the user to accept the responsability for a given productInShop.
  */
 angular.module('jDashboardFluxApp').controller('ProductChaseModalController', [
-    '$scope', '$modalInstance', '$window', 'productInShops', 'user', '$$sdkMailer',
-    function ($scope, $modalInstance, $window, productInShops, user, $$sdkMailer) {
+    '$scope', '$modalInstance', '$window', 'productInShops', 'user', '$$sdkMailer', '$$sdkCrud',
+    function ($scope, $modalInstance, $window, productInShops, user, $$sdkMailer, $$sdkCrud) {
 
-    // ------------------------------------------------------------------------
-    // Variables
-    // ------------------------------------------------------------------------
-        $scope.productInShops = productInShops;
-        $scope.message = {
-            from: user,
-            to: {
-                username: null,
-                organization: null
-            },
-            subject: '[INCO] ' + user.firstname + ' ' + user.lastname + ' vous suggère une correction',
-            data: {
-                productInShops: productInShops
-            }
+        var ChaseModal = function () {
+            $scope.productInShops = productInShops;
+            this.initMessage();
+            this.initScope();
         };
 
-    // ------------------------------------------------------------------------
-    // Event binding
-    // ------------------------------------------------------------------------
-        $scope.ok = function () {
+        ChaseModal.prototype.initScope = function () {
+            $scope.recipients = [];
+            $scope.ok = this.validForm;
+            $scope.cancel = this.closeModal;
+        };
+
+        ChaseModal.prototype.validForm = function () {
             var usernames = [];
             var organizations = [];
             for (var i = 0; i < $scope.recipients.length; i++) {
@@ -38,7 +31,7 @@ angular.module('jDashboardFluxApp').controller('ProductChaseModalController', [
                 }
             }
             $scope.message.to.username = _.uniq(usernames).join(',');
-            $scope.message.to.organization = _.uniq(organizations).join(',');
+            $scope.message.to.organization = organizations;
             $$sdkMailer.RetailerProductDataCompletionInvitationPost($scope.message).success(function (response) {
                 $window.alert('Le message a bien été envoyé.');
                 $modalInstance.close();
@@ -46,14 +39,40 @@ angular.module('jDashboardFluxApp').controller('ProductChaseModalController', [
                 $window.alert('Une erreur est survenue pendant l\'envoi de l\'email. Merci de réessayer ultérieurement ou de contacter notre support.');
             });
         };
-        $scope.cancel = function () {
+
+        ChaseModal.prototype.closeModal = function () {
             $modalInstance.dismiss('cancel');
         };
 
+        ChaseModal.prototype.initMessage = function () {
+            if (productInShops.length === 1) {
+                this.initProductContact();
+            }
+            $scope.message = {
+                from: user,
+                to: {
+                    username: null,
+                    organization: null
+                },
+                subject: '[INCO] ' + user.firstname + ' ' + user.lastname + ' vous suggère une correction',
+                data: {
+                    productInShops: productInShops
+                }
+            };
+        };
 
-    // ------------------------------------------------------------------------
-    // Init
-    // ------------------------------------------------------------------------
-        $scope.recipients = [{ name: 'Nestlé', value: 34 }];
+        ChaseModal.prototype.initProductContact = function () {
+            var productInShop = productInShops[0];
+            $$sdkCrud.ProductShow(productInShop.isIdentifiedBy[0].identifiesProduct.id).then(function (result) {
+                var product = result.data.data;
+                var brand = product.isBrandedBy;
+                // API returns 'ProductBrand' if the product has no brand
+                if (brand.name !== 'ProductBrand') {
+                    $scope.recipients.push({ type: 'organization', name: brand.name, value: brand.id });
+                }
+            });
+        };
+
+        var chaseModal = new ChaseModal();
     }
 ]);
